06e299bca046d291f1b562b901001e0525c457a1
mzmeyev <mzmeyev@sunpowercorp.com>
2022-03-09 20:31:52 +0200

    test message

---
+++ outPackage/someClass.java
@@ -0,0 +1,9 @@
+package outPackage;
+
+public class someClass {
+    public static void main(String[] args) {
+        String name = "name";
+        System.out.println("Hello World!" + name);
+    }
+
+}
